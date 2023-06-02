67a73095848157c02c6d24177464a0063d5b91bd
Gabriel Muñoz <93497614+mrGoonies@users.noreply.github.com>
2023-06-02 17:39:44 -0400

    Datos para renderizar en el template

--- home/views.py
+++ home/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import  HttpResponse
 
-# Create your views here.
+
+def sample(request):
+    dataset = {'books': [
+        {
+            'isbn': '9788499898803',
+            'title': 'Cien años de soledad',
+            'author': 'Gabriel García Márquez',
+            'publish': '1967',
+            'recommended': 'True'
+        },
+        {
+            'isbn': '9788420428946',
+            'title': '1984',
+            'author': 'George Orwell',
+            'publish': '1949',
+            'recommended': True
+        },
+        {
+            'isbn': '9788498382549',
+            'title': 'El código Da Vinci',
+            'author': 'Dan Brown',
+            'publish': '2003',
+            'recommended': False
+        }
+    ]}
+
+
+    return render(request, 'sample.html', dataset)
+
