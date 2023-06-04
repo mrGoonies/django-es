97f9825e21073733c28c963a269d7fc134167a27
Gabriel Muñoz <93497614+mrGoonies@users.noreply.github.com>
2023-06-04 14:28:46 -0400

    Registranod ubicación de los static files

--- Alexandria/urls.py
+++ Alexandria/urls.py
@@ -20,9 +20,11 @@ from django.urls import path, include
 # Primer
 from primer.views import say_hello
 
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='static/favicon.ico')),
     path('admin/', admin.site.urls),
     path('say_hello', say_hello),
     path('home/', include('home.urls')),
